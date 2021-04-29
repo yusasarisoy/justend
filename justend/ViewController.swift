465e32eb455a4e2e0d7e0095741c2a09ffe31e4c
Yuşa Sarısoy <yusasarisoy@icloud.com>
2021-04-29 14:45:29 +0300

    Initial Commit

---
+++ justend/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  justend
+//
+//  Created by Yuşa Sarısoy on 29.04.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
